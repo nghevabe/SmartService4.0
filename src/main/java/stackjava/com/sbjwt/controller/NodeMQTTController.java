d13fbd03df53d5cd864bec46463776f7c94a141a
nghevabe <30612768+nghevabe@users.noreply.github.com>
2020-08-12 22:05:44 +0700

    [Function] Create Api Publish Data to MQTT Broker part 2

--- src/main/java/stackjava/com/sbjwt/controller/NodeMQTTController.java
+++ src/main/java/stackjava/com/sbjwt/controller/NodeMQTTController.java
@@ -5,40 +5,108 @@ import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import stackjava.com.sbjwt.entities.HouseDeviceEntity;
+import stackjava.com.sbjwt.service.HouseDeviceService;
 
 @RestController
 @RequestMapping("/rest")
 public class NodeMQTTController {
 
+    @Autowired
+    HouseDeviceService houseDeviceService;
+
     @CrossOrigin
     @RequestMapping(value = "/publish_signal/{id}", method = RequestMethod.POST)
-    public ResponseEntity<String> publishData(@PathVariable int id, @RequestParam String signal) {
+    public ResponseEntity<String> publishData(@PathVariable int id, @RequestParam String signal
+            , @RequestParam(required=false) String lightColor, @RequestParam(required=false) String power) {
 
-        String message = "";
-
-        switch (signal) {
-            case "ON":
-                message = "ON0";
-                break;
-            case "OFF":
-                message = "OFF0";
-                break;
-            default:
-                return new ResponseEntity<String>("Invalid Signal", HttpStatus.CREATED);
+        if(lightColor==null){
+            lightColor="none";
         }
 
-            return sendMessage(message);
+        if(power==null){
+            power="none";
+        }
+
+        HouseDeviceEntity houseDeviceEntity = houseDeviceService.findById(id);
+
+        System.out.println("Topic: "+houseDeviceEntity.getTopic());
+        System.out.println("type: "+houseDeviceEntity.getType());
+
+        String topic = houseDeviceEntity.getTopic();
+        String type = houseDeviceEntity.getType();
+        String message = "";
+
+        if(type.equals("Fan") || type.equals("Glass") || type.equals("Door")) {
+
+            switch (signal) {
+                case "ON":
+                    message = "ON0";
+                    break;
+                case "OFF":
+                    message = "OFF0";
+                    break;
+                default:
+                    return new ResponseEntity<String>("Invalid Signal", HttpStatus.CREATED);
+            }
+
+        }
+
+        if(type.equals("Light")){
+            switch (signal) {
+                case "ON":
+                    // only me
+                    if(id == 505) {
+                        message = "2552552551";
+                    }
+                    if(id == 506){
+                        message = "2552552552";
+                    }
+                    if(id == 507){
+                        message = "2552552553";
+                    }
+                    // only me
+
+//                    // for public
+//                    message = "2552552551";
+//                    // for public
+
+                    break;
+                case "OFF":
+                    // only me
+                    if(id == 505) {
+                        message = "0000000001";
+                    }
+                    if(id == 506){
+                        message = "0000000002";
+                    }
+                    if(id == 507){
+                        message = "0000000003";
+                    }
+                    // only me
+
+//                    // for public
+//                    message = "0000000001";
+//                    // for public
+                    break;
+                default:
+                    return new ResponseEntity<String>("Invalid Signal", HttpStatus.CREATED);
+            }
+        }
+
+            return sendMessage(message,topic);
 
 
     }
 
-    public ResponseEntity<String> sendMessage(String mes){
+    public ResponseEntity<String> sendMessage(String message, String topic){
 
-        String topic        = "SMART_PROJECT/ESP_01";
-        String content      = mes;
+        //String topic        = "SMART_PROJECT/ESP_01";
+        //String content      = mes;
         int qos             = 0;
         String broker       = "tcp://tailor.cloudmqtt.com:11359";
         String clientId     = "JavaSample";
@@ -56,10 +124,10 @@ public class NodeMQTTController {
             System.out.println("Connecting to broker: "+broker);
             sampleClient.connect(connOpts);
             System.out.println("Connected");
-            System.out.println("Publishing message: "+content);
-            MqttMessage message = new MqttMessage(content.getBytes());
-            message.setQos(qos);
-            sampleClient.publish(topic, message);
+            System.out.println("Publishing message: "+message);
+            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+            mqttMessage.setQos(qos);
+            sampleClient.publish(topic, mqttMessage);
             System.out.println("Message published");
             sampleClient.disconnect();
             System.out.println("Disconnected");
@@ -69,7 +137,7 @@ public class NodeMQTTController {
             System.out.println("msg "+me.getMessage());
             System.out.println("loc "+me.getLocalizedMessage());
             System.out.println("cause "+me.getCause());
-            System.out.println("excep "+me);
+            System.out.println("except "+me);
             me.printStackTrace();
             return new ResponseEntity<String>("Fail", HttpStatus.CREATED);
         }
